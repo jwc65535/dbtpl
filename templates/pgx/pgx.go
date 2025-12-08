52add9c34365960bf401fbf178e1eccb0b1ecbd8
jwc65535 <36612276+jwc65535@users.noreply.github.com>
2025-12-08 14:20:27 -0500

    Fix logf helper when no columns are logged

--- templates/pgx/pgx.go
+++ templates/pgx/pgx.go
@@ -1564,7 +1564,9 @@ func (f *Funcs) logf_pkeys(v any) string {
 	p := []string{"sqlstr"}
 	switch x := v.(type) {
 	case Table:
-		p = append(p, f.names(f.short(x.GoName)+".", x.PrimaryKeys))
+		if names := f.names(f.short(x.GoName)+".", x.PrimaryKeys); names != "" {
+			p = append(p, names)
+		}
 	}
 	return fmt.Sprintf("logf(%s)", strings.Join(p, ", "))
 }
@@ -1590,7 +1592,9 @@ func (f *Funcs) logf(v any, ignore ...any) string {
 	// add fields
 	switch x := v.(type) {
 	case Table:
-		p = append(p, f.names_ignore(f.short(x.GoName)+".", x, ignoreNames...))
+		if names := f.names_ignore(f.short(x.GoName)+".", x, ignoreNames...); names != "" {
+			p = append(p, names)
+		}
 	default:
 		return fmt.Sprintf("[[ UNSUPPORTED TYPE 12: %T ]]", v)
 	}
@@ -1606,7 +1610,12 @@ func (f *Funcs) logf_update(v any) string {
 		for _, pk := range x.PrimaryKeys {
 			ignore = append(ignore, pk.GoName)
 		}
-		p = append(p, f.names_ignore(prefix, x, ignore...), f.names(prefix, x.PrimaryKeys))
+		if names := f.names_ignore(prefix, x, ignore...); names != "" {
+			p = append(p, names)
+		}
+		if names := f.names(prefix, x.PrimaryKeys); names != "" {
+			p = append(p, names)
+		}
 	default:
 		return fmt.Sprintf("[[ UNSUPPORTED TYPE 13: %T ]]", v)
 	}
